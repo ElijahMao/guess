baf554fd7f081965051ed6fe03f9d2497a100365
ElijahMao <40432292+ElijahMao@users.noreply.github.com>
2018-08-25 11:38:24 +0800

    20180817

---
+++ Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtGuess = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnAgain = New System.Windows.Forms.Button()
+        Me.btnEnd = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.Font = New System.Drawing.Font("新細明體", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(55, 38)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(319, 66)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblMsg
+        '
+        Me.lblMsg.Location = New System.Drawing.Point(41, 173)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(118, 43)
+        Me.lblMsg.TabIndex = 1
+        Me.lblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(41, 146)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 27)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "要猜的數字"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtGuess
+        '
+        Me.txtGuess.Location = New System.Drawing.Point(179, 146)
+        Me.txtGuess.Name = "txtGuess"
+        Me.txtGuess.Size = New System.Drawing.Size(100, 27)
+        Me.txtGuess.TabIndex = 3
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(58, 272)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 43)
+        Me.btnOk.TabIndex = 4
+        Me.btnOk.Text = "確定"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnAgain
+        '
+        Me.btnAgain.Location = New System.Drawing.Point(179, 272)
+        Me.btnAgain.Name = "btnAgain"
+        Me.btnAgain.Size = New System.Drawing.Size(75, 43)
+        Me.btnAgain.TabIndex = 6
+        Me.btnAgain.Text = "重玩"
+        Me.btnAgain.UseVisualStyleBackColor = True
+        '
+        'btnEnd
+        '
+        Me.btnEnd.Location = New System.Drawing.Point(299, 272)
+        Me.btnEnd.Name = "btnEnd"
+        Me.btnEnd.Size = New System.Drawing.Size(75, 43)
+        Me.btnEnd.TabIndex = 8
+        Me.btnEnd.Text = "結束"
+        Me.btnEnd.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(426, 349)
+        Me.Controls.Add(Me.btnEnd)
+        Me.Controls.Add(Me.btnAgain)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtGuess)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Font = New System.Drawing.Font("新細明體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "猜數字遊戲"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents lblMsg As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtGuess As System.Windows.Forms.TextBox
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents btnAgain As System.Windows.Forms.Button
+    Friend WithEvents btnEnd As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+
+    Dim guess, count, min, max As Integer
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim r As New Random()
+        guess = r.Next(1, 1000)
+        min = 1
+        max = 999
+        count = 0
+        lblTitle.Text = min.ToString() & " < ? < " & max.ToString()
+        lblMsg.Text = "共猜了 " & count.ToString() & "次"
+        btnOk.Enabled = True
+        txtGuess.Text = ""
+    End Sub
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+        count += 1
+        Dim myguess As Integer = Val(txtGuess.Text)
+
+        If myguess >= min And myguess < max Then
+            If myguess = guess Then
+                MessageBox.Show("賓果")
+                btnOk.Enabled = False
+            ElseIf myguess > guess Then
+                max = myguess
+                MessageBox.Show("再小一點")
+            ElseIf myguess < guess Then
+                min = myguess
+                MessageBox.Show("再大一點")
+            End If
+        Else
+            MessageBox.Show("輸入提示範圍內的數字")
+        End If
+        lblMsg.Text = "共猜了 " & count.ToString() & "次"
+        lblTitle.Text = min.ToString() & " < ? < " & max.ToString()
+    End Sub
+
+    Private Sub btnAgain_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAgain.Click
+        Form1_Load(sender, e)
+    End Sub
+
+    Private Sub btnEnd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEnd.Click
+        If MessageBox.Show("請問是否離開游戲嗎?", "猜數字遊戲", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) = Windows.Forms.DialogResult.OK Then
+            Application.Exit()
+
+        End If
+    End Sub
+End Class
